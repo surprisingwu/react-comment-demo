4916b1cafff5d48c191a49ebb1baf3a746ca8d96
surprisingwu <surprise.wu@foxmail.com>
2019-03-25 22:41:41 +0800

    峰会上客户反馈都是

--- src/common/Swipe/index.js
+++ src/common/Swipe/index.js
@@ -1,55 +1,72 @@
-import React, { Component } from 'react';
-import {
-  SwipeWrapper,
-  SwipeItem,
-  SwipeContent,
-  SwipeLeftArrow,
-  SwipeRightArrow,
-  SwipeDots
-} from './style'
+import React, { Component } from "react";
+import { SwipeWrapper, SwipeItem, SwipeContent } from "./style";
 
+// 一个移出动画
 export default class Swipe extends Component {
   constructor() {
-    super()
+    super();
     this.state = {
       list: [
-        { src: '//upload.jianshu.io/admin_banners/web_images/4620/8ce28ed4656eaa9d606d92c60ba6a04e419cf39b.png?imageMogr2/auto-orient/strip|imageView2/1/w/1250/h/540' },
-        { src: '//upload.jianshu.io/admin_banners/web_images/4622/e1304936b67030a9a597656889ec4b0bb98f50f0.jpg?imageMogr2/auto-orient/strip|imageView2/1/w/1250/h/540' },
-        {src: '//upload.jianshu.io/admin_banners/web_images/4592/22f5cfa984d47eaf3def6a48510cc87c157bf293.png?imageMogr2/auto-orient/strip|imageView2/1/w/1250/h/540'}
+        {
+          src:
+            "//upload.jianshu.io/admin_banners/web_images/4620/8ce28ed4656eaa9d606d92c60ba6a04e419cf39b.png?imageMogr2/auto-orient/strip|imageView2/1/w/1250/h/540"
+        },
+        {
+          src:
+            "//upload.jianshu.io/admin_banners/web_images/4622/e1304936b67030a9a597656889ec4b0bb98f50f0.jpg?imageMogr2/auto-orient/strip|imageView2/1/w/1250/h/540"
+        },
+        {
+          src:
+            "//upload.jianshu.io/admin_banners/web_images/4592/22f5cfa984d47eaf3def6a48510cc87c157bf293.png?imageMogr2/auto-orient/strip|imageView2/1/w/1250/h/540"
+        }
       ],
-      isMouseOn: false
-    }
-    this.handlerMouseEnter = this.handlerMouseEnter.bind(this)
-    this.handlerMouseLeave = this.handlerMouseLeave.bind(this)
+      activeIndex: 2
+    };
   }
-  handlerMouseEnter() { 
-    this.setState({
-      isMouseOn: true
-    })
-  }
-  handlerMouseLeave() { 
-    this.setState({
-      isMouseOn: false
-    })
+  componentDidMount() {
+    const children = this.els.children;
+    const len = children.length;
+    let lastIndex
+    const callBack = () => {
+      let { activeIndex } = this.state;
+      lastIndex = activeIndex
+      const currentEl = children[activeIndex];
+      currentEl.style.zIndex = '2'
+      currentEl.style.transform = `translate3d(-100%, 0, 0)`;
+      if (activeIndex >= len-1) {
+        activeIndex = 0;
+      } else {
+        activeIndex++;
+      }
+      if(lastIndex !== activeIndex ) {
+        const lastEl = children[activeIndex]
+        lastEl.style.zIndex = '-1'
+        lastEl.style.transform = `translate3d(0, 0, 0)`
+      }
+     
+      this.setState({
+        activeIndex
+      });
+    };
+    this.intervalTimer = setInterval(callBack, 5000);
   }
   render() {
-    const {list} = this.state
+    const { list } = this.state;
     return (
-      <SwipeWrapper onMouseEnter={this.handlerMouseEnter} onMouseLeave={this.handlerMouseLeave}>
-        {list.map((item, i) =>  
-          <SwipeItem key={i}>
+      <SwipeWrapper
+        ref={els => {
+          this.els = els;
+        }}
+      >
+        {" "}
+        {list.map((item, i) => (
+          <SwipeItem key={i} index={i}>
             <SwipeContent>
-              <img className='img' alt='' src={item.src}/>
-            </SwipeContent>
+              <img className="img" alt="" src={item.src} />{" "}
+            </SwipeContent>{" "}
           </SwipeItem>
-        )}
-        <SwipeLeftArrow />
-        <SwipeRightArrow />
-        <SwipeDots>
-        </SwipeDots>
+        ))}{" "}
       </SwipeWrapper>
-    )
+    );
   }
-
-
 }
--- src/common/Swipe/style.js
+++ src/common/Swipe/style.js
@@ -5,6 +5,7 @@ export const SwipeWrapper = styled.div`
   width: 100%;
   height: 100%;
   overflow: hidden;
+  z-index: 1;
 `;
 export const SwipeItem = styled.div`
   position: absolute;
@@ -12,6 +13,9 @@ export const SwipeItem = styled.div`
   top: 0;
   width: 100%;
   height: 100%;
+  transition: all 0.3s ease-in;
+  z-index: 1;
+  transform: translate3d(0,0,0);
 `;
 export const SwipeContent = styled.a.attrs({
   href: '#'
